48f701ba1b092188ab7b03df35f5e728e11dc810
FRYoussef <f.r.youssef@hotmail.com>
2020-04-10 11:01:19 +0200

    Added button interruption

--- arduino/src/Main/Main.ino
+++ arduino/src/Main/Main.ino
@@ -1,18 +1,35 @@
-const int button1Pin = 2;
-const int button2Pin = 3;
+#define EI_ARDUINO_INTERRUPTED_PIN
+#include <EnableInterrupt.h> //Thats for allow Int in all pins
+
+const int button1Pin = 4;
+const int button2Pin = 5;
+
+
+void buttonISR() {
+    switch (arduinoInterruptedPin) {
+        case button1Pin:
+            Serial.println("Button 1 is pressed");
+            break;
+        case button2Pin:
+            Serial.println("Button 2 is pressed");
+            break;
+   }
+
+   // for button bounce
+   delay(100);
+}
+
 
 void setup() {
     Serial.begin(9600);
     pinMode(button1Pin, INPUT);
     pinMode(button2Pin, INPUT);
+
+    enableInterrupt(button1Pin, buttonISR, CHANGE);
+    enableInterrupt(button2Pin, buttonISR, CHANGE);
 }
 
-void loop() {
-    if(digitalRead(button1Pin))
-      Serial.println("Button 1 is pressed");
 
-    if(digitalRead(button2Pin))
-      Serial.println("Button 2 is pressed");
-      
-    delay(150);
+void loop() {
+    delay(10000);
 }
