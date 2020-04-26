034c50fd1ca96abb3e0c30299ed064197bf93b53
raqblanc <raquel.blanco.morago@gmail.com>
2020-04-26 13:03:46 +0200

    Sensor thread + i2c

--- arduino/src/Main/Main.ino
+++ arduino/src/Main/Main.ino
@@ -1,6 +1,7 @@
 #define EI_ARDUINO_INTERRUPTED_PIN
 #include <EnableInterrupt.h> //Allow interruptions in all pins
-
+#include <Wire.h>
+#include <Thread.h>
 
 const int leftButtonPin = 4;
 const int rightButtonPin = 5;
@@ -8,6 +9,14 @@ const int triggerPin = 6;
 const int echoPin = 2;
 const int leftSolenoidPin = 9;
 const int rightSolenoidPin = 10;
+
+//Handle events
+const int START = 0;
+const int ACK_GOAL = 1;
+const int END = 2;
+const int ACK_START = 3;
+const int SCORE_UP = 4;
+const int AAK_END = 5;
 
 const char ERROR = -9;
 // here is why 58 -> https://www.instructables.com/id/Non-blocking-Ultrasonic-Sensor-for-Arduino/
@@ -17,8 +26,48 @@ volatile int time = 0;
 volatile boolean leftButton = false;
 volatile boolean rightButton = false;
 
+
+Thread sensorThread = Thread();
+int started = true; //set to false when raspberry works
+
 long startPulseTime = 0;
 
+void setup() {
+    // configure pin modes 
+    pinMode(leftButtonPin, INPUT);
+    pinMode(rightButtonPin, INPUT);
+    pinMode(echoPin, INPUT_PULLUP);
+    pinMode(triggerPin, OUTPUT);
+    pinMode(leftSolenoidPin, OUTPUT);
+    pinMode(rightSolenoidPin, OUTPUT);
+
+    Serial.begin(9600);
+
+    // link ISR functions
+    enableInterrupt(leftButtonPin, buttonISR, CHANGE);
+    enableInterrupt(rightButtonPin, buttonISR, CHANGE);
+    enableInterrupt(echoPin, echoISR, CHANGE);
+
+    // configure threads
+    sensorThread.onRun(sensorFunction);
+    sensorThread.setInterval(500);
+
+    //configure I2C
+    Wire.begin(0x8);
+    Wire.onReceive(receiveEvent);
+    //Wire.onRequest(sendData);
+}
+
+
+void sensorFunction(){
+
+    sendPulse();
+    int distance = (time / DISTANCE_CONVERSOR) + ERROR;
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println("cm");
+    
+}
 
 void buttonISR() {
     switch (arduinoInterruptedPin) {
@@ -51,31 +100,36 @@ void sendPulse(){
 }
 
 
-void setup() {
-    // configure pin modes 
-    pinMode(leftButtonPin, INPUT);
-    pinMode(rightButtonPin, INPUT);
-    pinMode(echoPin, INPUT_PULLUP);
-    pinMode(triggerPin, OUTPUT);
-    pinMode(leftSolenoidPin, OUTPUT);
-    pinMode(rightSolenoidPin, OUTPUT);
-
-    Serial.begin(9600);
-
-    // link ISR functions
-    enableInterrupt(leftButtonPin, buttonISR, CHANGE);
-    enableInterrupt(rightButtonPin, buttonISR, CHANGE);
-    enableInterrupt(echoPin, echoISR, CHANGE);
+void receiveEvent(int howMany) {
+  while (Wire.available()) { // loop through all but the last
+    int ev = Wire.read(); // receive byte as a character
+    switch(ev) {
+      case START:
+        started = true;
+        break;
+      case ACK_GOAL:
+        break;
+      case END:
+        break;
+    }
+  }
 }
 
+/*void sendData(){
+  Wire.write(number);
+}*/
 
 void loop() {
-    sendPulse();
-    int distance = (time / DISTANCE_CONVERSOR) + ERROR;
 
-    Serial.print("Distance: ");
-    Serial.print(distance);
-    Serial.println("cm");
+    while(!started) {} //Wait till START message
+    
+    Wire.write(ACK_START);
+
+    if(sensorThread.shouldRun())
+      sensorThread.run();
+   
+
+    
     
     delay(1000);
 }
