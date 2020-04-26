96eadeb73e07b90e5c77e64667a0a2ca37a1d2c5
FRYoussef <f.r.youssef@hotmail.com>
2020-04-26 18:41:11 +0200

    Added raspberry test file

---
+++ raspberry/bluetooth_test.py
@@ -0,0 +1,16 @@
+import bluetooth
+
+server_socket=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+port = 1
+server_socket.bind(("",port))
+server_socket.listen(1)
+client_socket,address = server_socket.accept()
+print("Accepted connection from ",address)
+
+while True:
+    res = client_socket.recv(1024)
+    client_socket.send(12)
+    print("Received: ",res)
+
+client_socket.close()
+server_socket.close()
